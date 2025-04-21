df3908692d0925908bc687a9bc4370bcbe2516d2
chang-jacob <69719427+chang-jacob@users.noreply.github.com>
2025-04-21 17:27:12 +0000

    Deploying to gh-pages from @ chang-jacob/chang-jacob.github.io@e296641e14556c27110364c24b28f8547ca2413a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-image-galleries",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
